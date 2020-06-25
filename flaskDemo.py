56848c3a86aedc383121e887049713a9dd108be8
lm197704 <123141061@qq.com>
2020-06-25 10:44:33 +0800

    flask简单应用及模板

--- flaskDemo.py
+++ flaskDemo.py
@@ -17,8 +17,14 @@ def index():
 def login():
     data = {'title': '登录页面', 'err_msg': '出错信息提示'}
     html = render_template('flaskdemo.html', **data)
+    #raise Exception('有异常！！！')
     return html
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=18000)
+    app.run(host='0.0.0.0',
+            port=18000,
+            debug=True,#开启调试模式
+            threaded=True,#开启多线程模式
+            processes=1#4个进程，默认只有一个进程，注意：theaded、processes不能同时开
+            )
